e3be18ca7c9b8769e1f8eef231a41b5eef3b6904
alejandroc77 <cabreraalejandro1828@gmail.com>
2022-08-20 18:55:32 -0500

    proyect at progress

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const { create } = require("express-handlebars");
+const app = express();
+const hbs = create({
+  extname: ".hbs",
+});
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+app.set("views", "./views");
+
+const port = 5000;
+
+app.get("/", (req, res) => {
+  res.render('home');
+});
+app.get("/login", (req, res) => {
+    res.render('login');
+  });
+app.use(express.static(__dirname + "public"));
+
+app.listen(5000, () => console.log(`Servidor escuchando en el puerto ${port}`));
